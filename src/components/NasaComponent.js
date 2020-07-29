af0873a0bc274356769012dda08067c102e52d1f
harvey magana <heyharv2012@gmail.com>
2020-07-28 21:25:36 -0700

    Altered the date of my nasa api call to see what would show.

--- src/components/NasaComponent.js
+++ src/components/NasaComponent.js
@@ -8,7 +8,7 @@ export default function NasaComponent () {
     const [things, setThings] = useState([])
 
     useEffect( () => {
-        axios.get("https://api.nasa.gov/planetary/apod?date=2020-07-28&api_key=JUeUMwskOxYmIzMB0ia5qheFd3nrwbTuqN1TBlLA")
+        axios.get("https://api.nasa.gov/planetary/apod?date=2020-07-27&api_key=JUeUMwskOxYmIzMB0ia5qheFd3nrwbTuqN1TBlLA")
             .then( response => {
 
                 setThings(response.data)
